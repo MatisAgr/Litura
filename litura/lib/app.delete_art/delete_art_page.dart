4b94f04fe9fe9b5bc79b15969fcf79867b2eb8ca
MatisAgr <matisagr@gmail.com>
2024-07-17 16:08:49 +0200

    feat: Ajouter la page de suppression d'oeuvre
    Cette modification ajoute la classe DeleteArtPage à l'application. Cette page permet aux utilisateurs de supprimer des oeuvres de la bibliothèque. La classe DeleteArtPage est une classe Stateless Widget qui affiche un espace réservé.

---
+++ litura/lib/app.delete_art/delete_art_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeleteArtPage extends StatelessWidget {
+  const DeleteArtPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

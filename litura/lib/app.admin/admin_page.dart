581bff5e0990d7dfc0b0ecdaff6c3be954e2a6e2
MatisAgr <matisagr@gmail.com>
2024-07-17 14:45:16 +0200

    feat: Add menu, admin, and search pages
    This commit adds the MenuPage, AdminPage, and SearchPage classes to the codebase. These pages are essential for the functionality of the application. The MenuPage provides a menu for navigation, the AdminPage allows administrative tasks, and the SearchPage enables users to search for books. This commit also includes the necessary imports and updates the MainPage to include these new pages.
    
    Refs: #123

---
+++ litura/lib/app.admin/admin_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatelessWidget {
+  const AdminPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- litura/lib/common_widgets/custom_book_card.dart
+++ litura/lib/common_widgets/custom_book_card.dart
@@ -1,10 +1,115 @@
 import 'package:flutter/material.dart';
 
 class CustomBookCard extends StatelessWidget {
-  const CustomBookCard({super.key});
+  final String imageUrl;
+  final String title;
+  final String category;
+  final double rating;
+
+  const CustomBookCard({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.category,
+    required this.rating,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      color: const Color(0xffb9848c),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.network(
+                imageUrl,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'FiraSans',
+                      ),
+                    ),
+                    Text(
+                      category,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        fontFamily: 'FiraSans',
+                      ),
+                    ),
+                    const SizedBox(height: 30.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Note: $rating',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 14.0,
+                                fontFamily: 'FiraSans',
+                              ),
+                            ),
+                            const Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                          ],
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            backgroundColor: const Color(0xff2f70af),
+                            minimumSize: const Size(110.0, 40.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                              side: const BorderSide(color: Colors.white),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                'Voir',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Numans',
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Icon(
+                                Icons.visibility,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

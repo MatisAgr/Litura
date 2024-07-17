f42e0e8335e4af6dc42aca0dafe1054ed8ea3b8b
MatisAgr <matisagr@gmail.com>
2024-07-17 12:03:19 +0200

    import font

---
+++ litura/lib/common_widgets/custom_book_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomBookCard extends StatelessWidget {
+  const CustomBookCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

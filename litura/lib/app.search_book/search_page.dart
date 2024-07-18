544dde7557c5dd886bbb618b37a639a1111aac4e
MatisAgr <matisagr@gmail.com>
2024-07-18 10:17:08 +0200

    Update database configuration to use default MariaDB port

--- litura/lib/app.search_book/search_page.dart
+++ litura/lib/app.search_book/search_page.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:litura/api/api_get.dart';
 
-class SearchPage extends StatelessWidget {
-  const SearchPage({super.key});
+class Loisir {
+  final String type;
+  final String nom;
+  final String image;
+  final double note;
+  final String description;
+
+  Loisir({
+    required this.type,
+    required this.nom,
+    required this.image,
+    required this.note,
+    required this.description,
+  });
+
+  factory Loisir.fromJson(Map<String, dynamic> json) {
+    return Loisir(
+      type: json['loisir_type'],
+      nom: json['loisir_nom'],
+      image: json['loisir_image'],
+      note: json['loisir_note'].toDouble(),
+      description: json['loisir_description'],
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<Loisir> _loisirs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchLoisirs();
+  }
+
+  void _fetchLoisirs() async {
+    final loisirsData = await Gets.getAllLoisir();
+    final loisirs = loisirsData.map<Loisir>((json) => Loisir.fromJson(json)).toList();
+    setState(() {
+      _loisirs = loisirs;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,24 +70,17 @@ class SearchPage extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
         color: const Color(0xFF2f70AF),
-        child: ListView(
-          children: const <Widget>[
-            Column(
-              children: [
-                SizedBox(height: 20.0),
-                Center(
-                  child: Text(
-                    'Rechercher un livre',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+        child: ListView.builder(
+          itemCount: _loisirs.length,
+          itemBuilder: (context, index) {
+            final loisir = _loisirs[index];
+            return ListTile(
+              title: Text(loisir.nom, style: const TextStyle(color: Colors.white)),
+              subtitle: Text(loisir.description, style: const TextStyle(color: Colors.white70)),
+              leading: Image.network(loisir.image, width: 50, height: 50),
+              trailing: Text('${loisir.note}/5'),
+            );
+          },
         ),
       ),
     );

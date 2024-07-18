4e3c046149ea0224ecd5be63ddc5858bbe410914
MatisAgr <matisagr@gmail.com>
2024-07-18 16:47:06 +0200

    chore: Update login page UI and button text

--- litura/lib/app.user/register_page.dart
+++ litura/lib/app.user/register_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:litura/app.search_book/search_page.dart';
 import 'package:litura/api/api_post.dart';
+import 'package:litura/common_widgets/custom_text_button.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -20,7 +21,7 @@ class _RegisterPageState extends State<RegisterPage> {
   String email = '';
   String password = '';
 
-  Future<void> signIn() async {
+  Future<void> registerIn() async {
     if (_formKey.currentState!.validate()) {
       setState(() {
         email = emailController.text;
@@ -28,15 +29,17 @@ class _RegisterPageState extends State<RegisterPage> {
         prenom = prenomController.text;
         password = passwordController.text;
       });
-  
+
       try {
-        Map<String, dynamic> user = await Posts.createUser(nom, prenom, email, password, 'user');
+        Map<String, dynamic> user =
+            await Posts.createUser(nom, prenom, email, password, 'user');
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text("Utilisateur créé avec succès : ${user.toString()}"),
           ),
         );
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage()));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const SearchPage()));
       } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
@@ -77,25 +80,82 @@ class _RegisterPageState extends State<RegisterPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
+                  style: const TextStyle(
+                    fontFamily: 'Numans',
+                  ),
                   controller: nomController,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff806491),
+                        width: 2.0,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey, // Couleur des bords par défaut
+                        width: 1.0,
+                      ),
+                    ),
                     hintText: "Nom",
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez rentrer un nom';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 10),
                 TextFormField(
+                  style: const TextStyle(
+                    fontFamily: 'Numans',
+                  ),
                   controller: prenomController,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
+                                        focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff806491),
+                        width: 2.0,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey, // Couleur des bords par défaut
+                        width: 1.0,
+                      ),
+                    ),
                     hintText: "Prénom",
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez rentrer un prénom';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 10),
                 TextFormField(
+                  style: const TextStyle(
+                    fontFamily: 'Numans',
+                  ),
                   controller: emailController,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
+                                        focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff806491),
+                        width: 2.0,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey, // Couleur des bords par défaut
+                        width: 1.0,
+                      ),
+                    ),
                     hintText: "Email",
                   ),
                   validator: (value) {
@@ -107,10 +167,25 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
                 const SizedBox(height: 10),
                 TextFormField(
+                  style: const TextStyle(
+                    fontFamily: 'Numans',
+                  ),
                   controller: passwordController,
                   obscureText: true,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
+                                        focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff806491),
+                        width: 2.0,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey, // Couleur des bords par défaut
+                        width: 1.0,
+                      ),
+                    ),
                     hintText: "Mot de passe",
                   ),
                   validator: (value) {
@@ -121,12 +196,12 @@ class _RegisterPageState extends State<RegisterPage> {
                   },
                 ),
                 const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    signIn();
-                  },
-                  child: const Text('S\'inscrire'),
-                ),
+                CustomTextButton(
+                  backgroundColor: const Color(0xff2f70AF),
+                  txt: 'Inscription',
+                  iconData: Icons.login,
+                  onPressed: registerIn,
+                )
               ],
             ),
           ),
